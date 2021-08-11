56d5e1c63d9181cedca1826a1531cce24e5e79bf
Leonardo Albrecht <leoalbrecht1@gmail.com>
2021-08-10 21:59:43 -0300

    primeirocommit

---
+++ lib/models/contact.dart
@@ -0,0 +1,16 @@
+class Contact {
+  final String name;
+  final String photo;
+  final String email;
+  final String phone;
+  bool isFavorite;
+  Contact({
+    required this.name,
+    required this.photo,
+    required this.email,
+    required this.phone,
+    this.isFavorite = false,
+
+  });
+
+}
---
+++ lib/provider/list_of_contacts.dart
@@ -0,0 +1,63 @@
+import 'package:agenda_de_contatos/models/contact.dart';
+
+List<Contact> listOfContacts = [
+  Contact(
+    name: "Nicole Costa",
+    photo: "assets/images/Nicole.jpg",
+    email:  "nicole.costa@gmail.com",
+    phone: "+55 51 11111-1111",
+    
+
+  ),
+  Contact (
+    name: "Camila",
+    photo: "assets/images/Camila.jpg",
+    email: "camila@gmail.com",
+    phone: "+55 51 22222-2222",
+    isFavorite: true,
+
+
+  ),
+  Contact(
+    name: "Paulo",
+    photo: "assets/images/Paulo.jpg",
+    email: "paulo@gmail.com",
+    phone: "+55 51 33333-3333",
+   ),
+    Contact(
+    name: "Lucas",
+    photo: "assets/images/Lucas.jpg",
+    email: "lucas@gmail.com",
+    phone: "+55 51 44444-4444",
+    isFavorite: true,
+   ),
+    Contact(
+    name: "Juliana",
+    photo: "assets/images/Juliana.jpg",
+    email: "juliana@gmail.com",
+    phone: "+55 51 55555-5555",
+   ),
+    Contact(
+    name: "Thomas",
+    photo: "assets/images/Thomas.jpg",
+    email: "Thomas@gmail.com",
+    phone: "+55 51 66666-6666",
+    isFavorite: true,
+   ),
+    Contact(
+    name: "Claudia",
+    photo: "assets/images/claudia.jpg",
+    email: "Claudia@gmail.com",
+    phone: "+55 51 77777-7777",
+    
+   ),
+    Contact(
+    name: "Marina",
+    photo: "assets/images/Marina.jpg",
+    email: "marina@gmail.com",
+    phone: "+55 51 88888-8888",
+   ),
+  
+
+
+];
